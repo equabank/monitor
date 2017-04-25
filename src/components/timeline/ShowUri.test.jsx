0ccf28cd8872285ca6a79fba1d1646d197d0dee6
Radim Daniel Panek <rdpanek@gmail.cz>
2017-04-25 23:52:02 +0200

    UnitTest: ShowUri: added

---
+++ src/components/timeline/ShowUri.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ShowUri from './ShowUri';
+
+describe('<ShowUri />', () => {
+
+  it('show Uri in input', () => {
+    const slotUri = "http://monitor.io";
+    const wrapper = shallow(<ShowUri slotUri={slotUri} />);
+    expect(wrapper.prop('value')).toBe(slotUri);
+  });
+
+
+});
