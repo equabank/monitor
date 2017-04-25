a82d7b10eca2c54bb6733b6024e597a5306ec258
Radim Daniel Panek <rdpanek@gmail.cz>
2017-04-25 21:16:14 +0200

    UnitTest: DeleteSlotButton: added

--- src/components/timeline/DeleteSlotButton.jsx
+++ src/components/timeline/DeleteSlotButton.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import DeleteForever from 'material-ui/svg-icons/action/delete-forever';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class DeleteSlotButton extends Component {
 
@@ -13,14 +14,17 @@ export default class DeleteSlotButton extends Component {
     };
 
     return (
-      <RaisedButton
-        label="DISCARD"
-        secondary={true}
-        style={styles.button}
-        icon={<DeleteForever />}
-        disabled={this.props.selectedSlotId == null ? true : false}
-        onTouchTap={this.props.openDeleteDialog}
-      />
+      <MuiThemeProvider>
+        <RaisedButton
+          id="deleteSlotButton"
+          label="DISCARD"
+          secondary={true}
+          style={styles.button}
+          icon={<DeleteForever />}
+          disabled={this.props.selectedSlotId == null ? true : false}
+          onClick={() => this.props.openDeleteDialog()}
+        />
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/timeline/DeleteSlotButton.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { spy } from 'sinon';
+import DeleteSlotButton from './DeleteSlotButton';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
+describe('<DeleteSlotButton />', () => {
+
+  it('disabled openDeleteDialog button when not selected any slot', () => {
+    const selectedSlotId = null;
+    const openDeleteDialog = {};
+    const button = mount(
+      <DeleteSlotButton
+        selectedSlotId={selectedSlotId}
+        openDeleteDialog={openDeleteDialog}
+      />
+    );
+    let disabledButton = button.find('button#deleteSlotButton').prop('disabled');
+    expect(disabledButton).toBeTruthy();
+  });
+
+
+  it('openDeleteDialog when select any slot', () => {
+    const selectedSlotId = 123;
+    let openDeleteDialog = spy();
+    const button = mount(
+      <DeleteSlotButton
+        selectedSlotId={selectedSlotId}
+        openDeleteDialog={openDeleteDialog}
+      />
+    );
+    button.find('button#deleteSlotButton').simulate('click');
+    expect(openDeleteDialog.calledOnce).toBeTruthy();
+  });
+
+});
