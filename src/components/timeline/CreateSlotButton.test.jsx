b93e2ac96a2de98ea17f0d5a9a1c703eb433de76
Radim Daniel Panek <rdpanek@gmail.cz>
2017-06-01 10:44:26 +0200

    Prettier: CreateSlotButton

--- src/components/timeline/CreateSlotButton.test.jsx
+++ src/components/timeline/CreateSlotButton.test.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { spy } from 'sinon';
-import CreateSlotButton from './CreateSlotButton';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import React from "react";
+import { mount } from "enzyme";
+import { spy } from "sinon";
+import CreateSlotButton from "./CreateSlotButton";
+import injectTapEventPlugin from "react-tap-event-plugin";
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-
-describe('<CreateSlotButton />', () => {
-
-  it('toogleModal', () => {
+describe("<CreateSlotButton />", () => {
+  it("toogleModal", () => {
     let toggleOpenModal = spy();
     const button = mount(
-      <CreateSlotButton
-        toggleOpenModal={toggleOpenModal}
-      />
+      <CreateSlotButton toggleOpenModal={toggleOpenModal} />
     );
-    button.find('button#createSlotModalButton').simulate('click');
+    button.find("button#createSlotModalButton").simulate("click");
     expect(toggleOpenModal.calledOnce).toBeTruthy();
   });
-
 });
