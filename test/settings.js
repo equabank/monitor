e3a2f59e3dbe84e0476f24d47513acf968d960db
Radim Daniel Panek <rdpanek@gmail.cz>
2017-09-18 13:09:02 +0200

    Settings endpoint: int tests: upgraded

--- test/settings.js
+++ test/settings.js
@@ -3,42 +3,40 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
 import server from "../server";
+import Moment from "moment";
 let should = chai.should();
-let lastStateGeneratorSlotValidatorAllow = false;
+let generatorSlotValidatorAllow = false;
+
+// Every put to elastic must be a new, for elastic response "successful": 1
+let message = "Integration test message, unique number: " + Date.now();
+let color = "Warning";
+let endTime = Moment().add(120, "seconds").format("YYYY-MM-DDTHH:mm:ss");
 
 chai.use(chaiHttp);
 
+// Elasticsearch by default refreshes each shard every 1s,
+// so the document will be available to search 1s after indexing it.
+// https://github.com/elastic/elasticsearch-js/issues/231
+function sleep(milliseconds) {
+  let start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+
 describe("Settings", () => {
-  describe("GET /api/settings", () => {
-    it("generatorSlotValidatorAllow exist", done => {
-      chai.request(server).get("/api/settings").end((err, res) => {
-        if (err) {
-          done(err);
-        }
-        res.should.have.status(200);
-
-        if (res.body.elastic.responses[0].status !== 404) {
-          let configValue = res.body.elastic.responses[0].hits.hits[0]._source;
-          chai
-            .expect(configValue)
-            .to.have.property("generatorSlotValidatorAllow");
-          lastStateGeneratorSlotValidatorAllow =
-            configValue.generatorSlotValidatorAllow;
-          done();
-        } else {
-          done(err);
-        }
-      });
-    });
-
-    it("generatorSlotValidatorAllow change state", done => {
+  describe("PUT /api/settings", () => {
+    it("change state", done => {
       chai
         .request(server)
         .put("/api/settings")
         .send({
-          generatorSlotValidatorAllow: lastStateGeneratorSlotValidatorAllow
-            ? false
-            : true
+          generatorSlotValidatorAllow: generatorSlotValidatorAllow,
+          message: message,
+          color: color,
+          endTime: endTime
         })
         .end((err, res) => {
           if (err) {
@@ -52,6 +50,82 @@ describe("Settings", () => {
           });
           done();
         });
+    });
+  });
+
+  describe("GET /api/settings", () => {
+    it("verify that the values are stored correctly", done => {
+      chai.request(server).get("/api/settings").end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        res.should.have.status(200);
+
+        if (res.body.elastic.responses[0].status !== 404) {
+          let configValue = res.body.elastic.responses[0].hits.hits[0]._source;
+          chai
+            .expect(configValue)
+            .to.have.property("generatorSlotValidatorAllow");
+          chai.expect(configValue).to.have.property("message");
+          chai.expect(configValue).to.have.property("color");
+          chai.expect(configValue).to.have.property("endTime");
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+
+    it("property generatorSlotValidatorAllow is false", done => {
+      sleep(1000);
+      chai.request(server).get("/api/settings").end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        res.should.have.status(200);
+
+        if (res.body.elastic.responses[0].status !== 404) {
+          let configValue = res.body.elastic.responses[0].hits.hits[0]._source;
+          chai.expect(configValue.generatorSlotValidatorAllow).to.be.false;
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+
+    it("property message contains text: " + message, done => {
+      chai.request(server).get("/api/settings").end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        res.should.have.status(200);
+
+        if (res.body.elastic.responses[0].status !== 404) {
+          let configValue = res.body.elastic.responses[0].hits.hits[0]._source;
+          chai.expect(configValue.message).to.be.eql(message);
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+
+    it("property endTime contains: " + endTime, done => {
+      chai.request(server).get("/api/settings").end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        res.should.have.status(200);
+
+        if (res.body.elastic.responses[0].status !== 404) {
+          let configValue = res.body.elastic.responses[0].hits.hits[0]._source;
+          chai.expect(configValue.endTime).to.be.eql(endTime);
+          done();
+        } else {
+          done(err);
+        }
+      });
     });
   });
 });
